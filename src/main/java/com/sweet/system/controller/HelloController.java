c346aead1e3c18cf8243816dea8de5ea44b28274
459699496@qq.com <ldl102000>
2019-06-13 17:58:52 +0800

    项目初始化

---
+++ src/main/java/com/sweet/system/controller/HelloController.java
@@ -0,0 +1,1 @@
+package com.sweet.system.controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RequestMapping("/hello")@RestControllerpublic class HelloController {    @RequestMapping("")    public String hello(){         return "hello sweet!";    }}

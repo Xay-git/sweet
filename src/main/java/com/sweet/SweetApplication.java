5905ddfcd71abbb7f7f861b920170f62d83ad4d6
459699496@qq.com <ldl102000>
2019-06-20 11:24:17 +0800

    修改代码生成

--- src/main/java/com/sweet/SweetApplication.java
+++ src/main/java/com/sweet/SweetApplication.java
@@ -3,9 +3,10 @@ package com.sweet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.sweet.*")
+@MapperScan("com.sweet.*.*.mapper")//这个注解，作用相当于下面的@Bean MapperScannerConfigurer，2者配置1份即可
 public class SweetApplication {
 
     public static void main(String[] args) {

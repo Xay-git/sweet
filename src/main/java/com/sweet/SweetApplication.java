eb2c8f3a70e148f7fa664de0458afdae7d74e0e0
459699496@qq.com <ldl102000>
2019-06-21 08:42:45 +0800

    集成swagger

--- src/main/java/com/sweet/SweetApplication.java
+++ src/main/java/com/sweet/SweetApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.sweet.*.*.mapper")//这个注解，作用相当于下面的@Bean MapperScannerConfigurer，2者配置1份即可
 public class SweetApplication {
 
     public static void main(String[] args) {

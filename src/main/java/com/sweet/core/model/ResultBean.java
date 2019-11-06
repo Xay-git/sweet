64b8f194f31b80f550272ace9ebcb92a662b8ccd
459699496@qq.com <ldl102000>
2019-11-06 09:58:33 +0800

    集成swagger

--- src/main/java/com/sweet/core/model/ResultBean.java
+++ src/main/java/com/sweet/core/model/ResultBean.java
@@ -10,25 +10,56 @@ public class ResultBean<T> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty("返回的信息")
-	private String msg = "success";
+	public static final String DEFAULT_SUCCESS_MESSAGE = "success";
+
+	public static final String DEFAULT_ERROR_MESSAGE = "error";
+
+	public static final Integer DEFAULT_SUCCESS_CODE = 200;
+
+	public static final Integer DEFAULT_ERROR_CODE = 500;
+
 	@ApiModelProperty("返回的状态码")
-	private int code = 200;
+	private int code;
+	@ApiModelProperty("返回的信息")
+	private String msg;
 	@ApiModelProperty("返回的数据")
 	private T data;
 
-	public ResultBean() {
-		super();
-	}
 
-	public ResultBean(T data) {
-		super();
+	public ResultBean( int code, String msg,T data) {
+		this.msg = msg;
+		this.code = code;
 		this.data = data;
 	}
 
-	public ResultBean(Throwable e) {
-		super();
-		this.msg = e.toString();
-		this.code = 200;
+	public static ResultSuccess success() {
+		return new ResultSuccess();
 	}
+
+	public static ResultSuccess success(Object object) {
+		return new ResultSuccess(object);
+	}
+
+	public static ResultSuccess success(Integer code, String message, Object object) {
+		return new ResultSuccess(code, message, object);
+	}
+
+	public static ResultError error(String message) {
+		return new ResultError(message);
+	}
+
+	public static ResultError error(Integer code, String message) {
+		return new ResultError(code, message);
+	}
+
+	public static ResultError error(Integer code, String message, Object object) {
+		return new ResultError(code, message, object);
+	}
+
+
+
+
+
+
+
 }

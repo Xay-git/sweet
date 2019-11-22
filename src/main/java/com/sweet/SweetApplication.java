1b57fd4784f537f51804ea37a1b9012ce42dc3a0
459699496@qq.com <ldl102000>
2019-11-22 16:17:27 +0800

    集成shiro

--- src/main/java/com/sweet/SweetApplication.java
+++ src/main/java/com/sweet/SweetApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync //开启异步调用
 public class SweetApplication {
 
     public static void main(String[] args) {

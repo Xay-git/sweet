a2e697e951ff4a19254fb1d7b1050ce39bc87719
459699496@qq.com <ldl102000>
2019-12-03 18:50:37 +0800

    集成shiro

--- sql/sweet.sql
+++ sql/sweet.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50710
 File Encoding         : 65001
 
-Date: 2019-11-29 17:45:27
+Date: 2019-12-03 18:49:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -59,8 +59,8 @@ CREATE TABLE `t_dept` (
 -- ----------------------------
 -- Records of t_dept
 -- ----------------------------
-INSERT INTO `t_dept` VALUES ('24c43877d7cc4e1a4b7957a4324843a3', '0', 'NBA', null, '2019-11-29 16:42:51', null, '0');
-INSERT INTO `t_dept` VALUES ('61d59fc36dfd9e0f8d477faa2b3f545f', '24c43877d7cc4e1a4b7957a4324843a3', '湖人队', null, '2019-11-29 16:43:01', null, '0,24c43877d7cc4e1a4b7957a4324843a3');
+INSERT INTO `t_dept` VALUES ('24c43877d7cc4e1a4b7957a4324843a3', '0', 'NBA', '1', '2019-11-29 16:42:51', '2019-12-03 15:16:11', '0');
+INSERT INTO `t_dept` VALUES ('61d59fc36dfd9e0f8d477faa2b3f545f', '24c43877d7cc4e1a4b7957a4324843a3', '湖人队', '2', '2019-11-29 16:43:01', '2019-12-03 15:15:28', '0,24c43877d7cc4e1a4b7957a4324843a3');
 
 -- ----------------------------
 -- Table structure for t_file_info
@@ -106,89 +106,81 @@ CREATE TABLE `t_menu` (
 -- ----------------------------
 -- Records of t_menu
 -- ----------------------------
-INSERT INTO `t_menu` VALUES ('2143ca29d50ce8fe877a6b1589ccd50b', '0', '未命名', null, null, null, '0', null, '2019-11-28 10:28:34', null);
-INSERT INTO `t_menu` VALUES ('4e22cef5667769e1e7ef38a4f7fdd621', '0', '未命名', null, null, null, '0', null, '2019-11-28 10:27:30', null);
-INSERT INTO `t_menu` VALUES ('535c9e82f95efad034120ebb459c464d', '0', '系统管理', null, 'user:system', 'layui-icon-face-surprised', '0', '1', '2019-11-25 16:19:57', null);
-INSERT INTO `t_menu` VALUES ('6aaab46f304dc3cb60d8d769e3707b15', '535c9e82f95efad034120ebb459c464d', '菜单管理', null, null, 'layui-icon-snowflake', '0', '2', '2019-11-27 18:45:27', null);
-INSERT INTO `t_menu` VALUES ('d9a9fee6994e62a019dd69583f175272', '0', '开发工具', null, 'user:dev', 'layui-icon-refresh-1', '0', '1', '2019-11-27 18:43:00', null);
-INSERT INTO `t_menu` VALUES ('f78e6e398b8e369621f02fb929eb4015', '0', '测试菜单', null, null, 'layui-icon-login-weibo', '0', '3', '2019-11-27 18:43:41', null);
-
--- ----------------------------
--- Table structure for t_permission
--- ----------------------------
-DROP TABLE IF EXISTS `t_permission`;
-CREATE TABLE `t_permission` (
-  `ID` varchar(32) NOT NULL,
-  `NAME` varchar(255) DEFAULT NULL,
-  `URL` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of t_permission
--- ----------------------------
-INSERT INTO `t_permission` VALUES ('1', 'user:user', '/user');
-INSERT INTO `t_permission` VALUES ('2', 'user:add', '/user/add');
-INSERT INTO `t_permission` VALUES ('3', 'user:delete', '/user/delete');
+INSERT INTO `t_menu` VALUES ('0bf58ce456662b9f186683e41df1d92d', '5ad3caa634c441a58118ec2ffb424ee7', '部门管理', '/admin/dept', null, null, '0', '2', '2019-12-02 11:05:45', null);
+INSERT INTO `t_menu` VALUES ('0c6bce3dd7d7a352acfbfcba16617709', '5ad3caa634c441a58118ec2ffb424ee7', '菜单管理', '/admin/menu', null, null, '0', '1', '2019-12-02 11:02:38', null);
+INSERT INTO `t_menu` VALUES ('16446826125a42216f692db7db5fd91c', '5ad3caa634c441a58118ec2ffb424ee7', '用户管理', '/admin/user', null, null, '0', '3', '2019-12-02 11:09:16', null);
+INSERT INTO `t_menu` VALUES ('1649ebe3eeb624b25e5914d8b1ce904b', '5ad3caa634c441a58118ec2ffb424ee7', '角色管理', '/admin/role', null, null, '0', '4', '2019-12-02 11:09:38', null);
+INSERT INTO `t_menu` VALUES ('1f4efbedfbed0c9b1c8d6f56ea789f91', '881bac77e0690244cba83867665ffb5a', '代码生成', '/admin/gen', null, null, '0', '1', '2019-12-02 11:34:42', null);
+INSERT INTO `t_menu` VALUES ('3ba68944d827f39dfa13f5e9a6d91fdb', '5ad3caa634c441a58118ec2ffb424ee7', '后台管理主页', '/admin', null, null, '0', '5', '2019-12-03 18:42:42', null);
+INSERT INTO `t_menu` VALUES ('5ad3caa634c441a58118ec2ffb424ee7', '0', '系统管理', null, null, 'layui-icon-set', '0', '1', '2019-12-02 10:40:33', '2019-12-03 15:19:55');
+INSERT INTO `t_menu` VALUES ('881bac77e0690244cba83867665ffb5a', '0', '开发工具', null, null, 'layui-icon-util', '0', '2', '2019-12-02 11:34:22', null);
 
 -- ----------------------------
 -- Table structure for t_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role`;
 CREATE TABLE `t_role` (
-  `ID` varchar(32) NOT NULL,
+  `ROLE_ID` varchar(32) NOT NULL,
   `NAME` varchar(255) DEFAULT NULL COMMENT '角色名称',
   `MEMO` varchar(255) DEFAULT NULL COMMENT '角色描述',
-  PRIMARY KEY (`ID`)
+  `CREATE_TIME` datetime DEFAULT NULL,
+  PRIMARY KEY (`ROLE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
-INSERT INTO `t_role` VALUES ('1', 'admin', '超级管理员');
-INSERT INTO `t_role` VALUES ('2', 'test', '测试账户');
+INSERT INTO `t_role` VALUES ('1', 'admin', '超级管理员', '2019-12-02 10:23:49');
+INSERT INTO `t_role` VALUES ('2', 'test', '测试账户', '2019-12-02 10:23:52');
+INSERT INTO `t_role` VALUES ('a168a6a2220399254b46045241905296', '老板', '老板', '2019-12-03 15:55:26');
 
 -- ----------------------------
--- Table structure for t_role_permission
+-- Table structure for t_role_menu
 -- ----------------------------
-DROP TABLE IF EXISTS `t_role_permission`;
-CREATE TABLE `t_role_permission` (
+DROP TABLE IF EXISTS `t_role_menu`;
+CREATE TABLE `t_role_menu` (
   `ID` varchar(32) NOT NULL,
   `RID` varchar(32) DEFAULT NULL COMMENT '角色ID',
-  `PID` varchar(32) DEFAULT NULL,
+  `MID` varchar(32) DEFAULT NULL COMMENT '菜单ID',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_role_permission
+-- Records of t_role_menu
 -- ----------------------------
-INSERT INTO `t_role_permission` VALUES ('1', '1', '1');
-INSERT INTO `t_role_permission` VALUES ('2', '1', '2');
-INSERT INTO `t_role_permission` VALUES ('3', '1', '3');
-INSERT INTO `t_role_permission` VALUES ('4', '2', '1');
+INSERT INTO `t_role_menu` VALUES ('04e38aff27af6a913d97664d22a681da', '1', '16446826125a42216f692db7db5fd91c');
+INSERT INTO `t_role_menu` VALUES ('1144516d369343ac00a2cb425acda17d', '1', '5ad3caa634c441a58118ec2ffb424ee7');
+INSERT INTO `t_role_menu` VALUES ('1c1d729440f2fcde5e64c06cd921494f', '2', '881bac77e0690244cba83867665ffb5a');
+INSERT INTO `t_role_menu` VALUES ('2f9401575ba891d036ae96972d8d9c65', '1', '1649ebe3eeb624b25e5914d8b1ce904b');
+INSERT INTO `t_role_menu` VALUES ('35ff13df852f99f125d839f38eefd83f', '2', '0bf58ce456662b9f186683e41df1d92d');
+INSERT INTO `t_role_menu` VALUES ('393f65ad04edb8cf76c3426d068b503c', '1', '0bf58ce456662b9f186683e41df1d92d');
+INSERT INTO `t_role_menu` VALUES ('4c3d47c3a5de22a2cab740282417333d', '2', '3ba68944d827f39dfa13f5e9a6d91fdb');
+INSERT INTO `t_role_menu` VALUES ('6e9373f68e2c3c20ed77568524bd6b90', 'a168a6a2220399254b46045241905296', '16446826125a42216f692db7db5fd91c');
+INSERT INTO `t_role_menu` VALUES ('c344df38c05f81f34e08b4b6bcaed200', 'a168a6a2220399254b46045241905296', '5ad3caa634c441a58118ec2ffb424ee7');
+INSERT INTO `t_role_menu` VALUES ('d5ca5e9cb438e4d58d2932c37a9ca4e0', '1', '0c6bce3dd7d7a352acfbfcba16617709');
+INSERT INTO `t_role_menu` VALUES ('e71ce685114a84a0f7f9dba66da90ce6', '2', '1f4efbedfbed0c9b1c8d6f56ea789f91');
+INSERT INTO `t_role_menu` VALUES ('f9a65fdcb4dffe52cc1a60310cd32021', '2', '5ad3caa634c441a58118ec2ffb424ee7');
 
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
-  `ID` varchar(32) NOT NULL,
+  `USER_ID` varchar(32) NOT NULL,
   `USER_NAME` varchar(255) DEFAULT NULL COMMENT '用户名',
   `PASSWORD` varchar(255) DEFAULT NULL COMMENT '密码',
   `BIRTHDAY` datetime DEFAULT NULL COMMENT '出生日期',
   `ACCOUNT_STATUS` varchar(255) DEFAULT NULL COMMENT '用户状态',
   `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
   `DEPT_ID` varchar(32) DEFAULT NULL COMMENT '部门Id',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'admin', 'c7242b2bc2435c1ead6b89f85d9e6a96', null, '1', null, null);
-INSERT INTO `t_user` VALUES ('840dbb43e8154d7934a4216fdb5d4ba9', 'lebron', '01ea695061b338a72ff4cbfb9be15e1d', '2019-11-29 00:00:00', '1', '2019-11-29 17:30:41', '61d59fc36dfd9e0f8d477faa2b3f545f');
-INSERT INTO `t_user` VALUES ('b9c8823c9f171a1c715bbfc51f6e4aea', 'admn', '9c7fc4d69a6612fb26077360a38b7cd5', '2019-11-29 00:00:00', '1', '2019-11-29 17:03:18', '24c43877d7cc4e1a4b7957a4324843a3');
-INSERT INTO `t_user` VALUES ('c44207743115b2547750be8bc82ea321', 'kobe', 'e283671f405c40d88b9017e5d07f3a97', '2019-11-29 00:00:00', '1', '2019-11-29 17:30:34', '61d59fc36dfd9e0f8d477faa2b3f545f');
+INSERT INTO `t_user` VALUES ('1', 'admin', 'c7242b2bc2435c1ead6b89f85d9e6a96', '2019-12-03 00:00:00', '1', '2019-12-03 15:14:39', '24c43877d7cc4e1a4b7957a4324843a3');
+INSERT INTO `t_user` VALUES ('d6c636ecf1790eaa3d9cf8a8c7c728da', 'Pope', '0703758e3ad97ed11f3df58619193c41', '2019-12-03 00:00:00', '1', '2019-12-03 15:42:05', '61d59fc36dfd9e0f8d477faa2b3f545f');
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -204,5 +196,6 @@ CREATE TABLE `t_user_role` (
 -- ----------------------------
 -- Records of t_user_role
 -- ----------------------------
-INSERT INTO `t_user_role` VALUES ('1', '1', '1');
-INSERT INTO `t_user_role` VALUES ('2', '2', '2');
+INSERT INTO `t_user_role` VALUES ('131559e4c14238cbbc1a4c133a8c9b1b', '1', '1');
+INSERT INTO `t_user_role` VALUES ('2f51c59f845700a97eb32420c47e0298', 'd6c636ecf1790eaa3d9cf8a8c7c728da', '2');
+INSERT INTO `t_user_role` VALUES ('55de843eb318bab914218a775c3a9d79', '1', '2');

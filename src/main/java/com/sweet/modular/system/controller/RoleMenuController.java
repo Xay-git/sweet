236bf2a9f745959df4b210f7f7b0af84ecb3086d
459699496@qq.com <ldl102000>
2019-12-03 17:26:14 +0800

    集成shiro

--- src/main/java/com/sweet/modular/system/controller/RoleMenuController.java
+++ src/main/java/com/sweet/modular/system/controller/RoleMenuController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-11-20
  */
 @RestController
-@RequestMapping("/rolePermission/role-permission")
+@RequestMapping("/admin/rolePermission")
 public class RoleMenuController {
 
 }
--- src/main/java/com/sweet/modular/system/controller/UserRoleController.java
+++ src/main/java/com/sweet/modular/system/controller/UserRoleController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-11-20
  */
 @RestController
-@RequestMapping("/userRole")
+@RequestMapping("/admin/userRole")
 public class UserRoleController {
 
 

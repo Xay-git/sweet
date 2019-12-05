8627dfa389e2f9c6741778ec4149dbf663f0f97a
459699496@qq.com <ldl102000>
2019-12-05 17:20:24 +0800

    集成shiro

--- src/main/java/com/sweet/modular/system/service/FileService.java
+++ src/main/java/com/sweet/modular/system/service/FileService.java
@@ -1,1 +1,1 @@
-package com.sweet.modular.system.service;import org.apache.tomcat.jni.FileInfo;import org.springframework.web.multipart.MultipartFile;public interface FileService {    public FileInfo uploadFile(MultipartFile file);}
+package com.sweet.modular.system.service;import com.sweet.modular.system.model.UploadResult;import org.apache.tomcat.jni.FileInfo;import org.springframework.web.multipart.MultipartFile;public interface FileService {    public UploadResult uploadFile(MultipartFile file, String fileSavePath);}
